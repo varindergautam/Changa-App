cb85f61c2dc48a2b23ad63a6ba872f5ab2a3fc4c
varindergautam <varindergautam48@gmail.com>
2023-06-06 00:51:38 +0530

    env port

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ io.on("connection", (socket) => {
 });
 
 // Start the server
-const port = 3000;
+const port = process.env.SOCKET_PORT;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
