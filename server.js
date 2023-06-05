40d37fc4e4cf7bb9df9964b376eedaf357a64042
varindergautam <varindergautam48@gmail.com>
2023-06-06 00:40:33 +0530

    port

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ io.on("connection", (socket) => {
 });
 
 // Start the server
-const port = 3000;
+const port = 443;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
