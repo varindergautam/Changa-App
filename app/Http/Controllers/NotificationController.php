f7fbd746e57c25d32421086d90ffa6e024736511
varindergautam <varindergautam48@gmail.com>
2023-06-15 15:45:05 +0530

    remove phone required in social login

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -11,20 +11,21 @@ class NotificationController extends Controller
 
     public function sendSmsNotificaition($phone, $otp)
     {
-        $basic  = new Basic('4c89a126', '0ondH8X33yXTABRE');
-        $client = new Client($basic);
- 
-        $response = $client->sms()->send(
-            new \Vonage\SMS\Message\SMS($phone, 'Widget Hubs', 'Your One time otp is '.$otp.' valid for 10 mints')
-        );
-        
-        $message = $response->current();
-        
-        if ($message->getStatus() == 0) {
-            return "The message was sent successfully";
-        } else {
-            return "The message failed with status: " . $message->getStatus();
+        if (!empty($phone)) {
+            $basic  = new Basic('4c89a126', '0ondH8X33yXTABRE');
+            $client = new Client($basic);
+
+            $response = $client->sms()->send(
+                new \Vonage\SMS\Message\SMS($phone, 'Widget Hubs', 'Your One time otp is ' . $otp . ' valid for 10 mints')
+            );
+
+            $message = $response->current();
+
+            if ($message->getStatus() == 0) {
+                return "The message was sent successfully";
+            } else {
+                return "The message failed with status: " . $message->getStatus();
+            }
         }
     }
 }
-
