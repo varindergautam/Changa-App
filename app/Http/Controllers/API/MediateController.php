4ba333b34cbccd6f3cf89f38a04b732694cccfa2
varindergautam <varindergautam48@gmail.com>
2023-07-09 14:24:08 +0530

    mediate listing issue

--- app/Http/Controllers/API/MediateController.php
+++ app/Http/Controllers/API/MediateController.php
@@ -50,9 +50,9 @@ class MediateController extends BaseController
                 $users[$key]['file'] = asset('/storage/file/'. $mediate->file);
                 $users[$key]['created_date'] = ChangaAppHelper::dateFormat($mediate->created_at);
                 $users[$key]['url'] = url('/api/mediate/mediate?id=' . $mediate->id);
-                $users[$key]['user']['profile_pic'] = !empty($mediate->user->profile_pic) ? asset('/storage/profile_pic/'. $mediate->user->profile_pic) : null;
+                $users[$key]['user']['profile_pic'] = isset($mediate->user) && !empty($mediate->user->profile_pic) ? asset('/storage/profile_pic/'. $mediate->user->profile_pic) : null;
 
-                $users[$key]['user']['background_image'] = !empty($mediate->user->background_image) ? asset('/storage/file/'. $mediate->user->background_image) : null;
+                $users[$key]['user']['background_image'] = isset($mediate->user) && !empty($mediate->user->background_image) ? asset('/storage/file/'. $mediate->user->background_image) : null;
 
                 $arr = [];
                 foreach($mediate->mediateTagMulti as $tag) {
